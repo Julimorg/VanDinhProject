a2823acb551e31e7f680ab0ec726b7041f07e3a7
Tran Kien Phong <kienphongtran2003@gmail.com>
2025-12-30 14:34:17 +0700

    change endpoint again!!!!

--- USER/src/Utils/env_dev_handler.ts
+++ USER/src/Utils/env_dev_handler.ts
@@ -1,6 +1,6 @@
 
 export const COOKIE_EXPIRE = import.meta.env.VITE_COOKIE_DEV_EXPIRE;
-export const PUBLIC_API = import.meta.env.VITE_DEV_LOCAL;
+export const PUBLIC_API = import.meta.env.VITE_DEV_API_URL;
 export const LOCAL_API = import.meta.env.VITE_DEV_LOCAL;
 export const LOCAL_API_RAW = import.meta.env.VITE_DEV_LOCAL;
 export const PUBLIC_API_RAW = import.meta.env.VITE_DEV_API_RAW;
